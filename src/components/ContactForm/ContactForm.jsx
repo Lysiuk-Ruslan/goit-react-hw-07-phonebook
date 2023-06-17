a58c682a681c2d402bca999a5eee08c1164fdf6e
Lisiuk-Ruslan <114481433+Lysiuk-Ruslan@users.noreply.github.com>
2023-06-17 17:13:47 +0300

    Клонування ДЗ

---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,66 @@
+import { useState } from 'react';
+import { Form, InputName, Input, AddContact } from './ContactForm.styled';
+import { useDispatch, useSelector } from 'react-redux';
+import { getContacts } from 'redux/selectors';
+import { addContact } from 'redux/contactsSlice';
+
+const ContactForm = () => {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+
+  const contacts = useSelector(getContacts);
+  const dispatch = useDispatch();
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (contacts.some(e => e.name === name)) {
+      alert(`${name} is already in contacts`);
+    } else {
+      dispatch(addContact(name, number));
+    }
+    setName('');
+    setNumber('');
+  };
+
+  const handleChange = evt => {
+    const { name, value } = evt.target;
+    switch (name) {
+      case 'name':
+        setName(value);
+        break;
+      case 'number':
+        setNumber(value);
+        break;
+      default:
+        return;
+    }
+  };
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <InputName>Name</InputName>
+      <Input
+        type="text"
+        value={name}
+        onChange={handleChange}
+        name="name"
+        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+        required
+      />
+      <InputName>Number</InputName>
+      <Input
+        type="tel"
+        value={number}
+        onChange={handleChange}
+        name="number"
+        pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+        title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+        required
+      />
+      <AddContact type="submit">Add contact</AddContact>
+    </Form>
+  );
+};
+
+export default ContactForm;
